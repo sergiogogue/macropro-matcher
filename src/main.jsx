9af9c4a168ce66cf35880e2c48a03fb9d37c30a2
sergiogogue <sgonzalez7@me.com>
2026-03-07 14:21:01 -0600

    Agregar filtros cascada inventario

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './MacroPro_Matcher.jsx'
+import App from './MacroPro_Filtros_Cascada.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
